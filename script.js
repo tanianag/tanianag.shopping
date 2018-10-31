0ddb5cd00372a9268ecbb2e48eb4222029e5fd89
brauvocera <brau@vocera.com>
2018-10-30 21:55:47 -0700

    Updating to ES2015

--- script.js
+++ script.js
@@ -1,98 +1,88 @@
- var button = document.getElementById("enter");
- var input = document.getElementById("userinput");
- var ul = document.querySelector("ul");
- var listItems = document.getElementsByTagName("LI");
- var deleteBtn = document.getElementsByTagName("button");
- 
-//get length of list 
+const button = document.getElementById("enter");
+const input = document.getElementById("userinput");
+const ul = document.querySelector("ul");
+const listItems = document.getElementsByTagName("LI");
+const deleteBtn = document.getElementsByTagName("button");
 
- function listLength() {
- 	console.log(listItems.length);
- 	return listItems.length
- }
+//get length of list
+
+const listLength = () => {
+  console.log(listItems.length);
+  return listItems.length;
+};
 
 //create delete buttons for exisiting buttons
 
- for (i = 0; i < listLength(); i++ ) {
-    var btn = document.createElement("button");
- 		    btn.appendChild(document.createTextNode("Delete!"));
- 		    listItems[i].appendChild(btn);
- 		    btn.onclick = removeParent;
-  	}
-//remove an item from the list 
+listLength.forEach(() => {
+  const btn = document.createElement("button");
+  btn.appendChild(document.createTextNode("Delete!"));
+  listItems[i].appendChild(btn);
+  btn.onclick = removeParent;
+});
 
-function removeParent(evt) {
-	evt.target.removeEventListener("click", removeParent, false);
-	evt.target.parentNode.remove();
-}
+//remove an item from the list
+
+const removeParent = evt => {
+  evt.target.removeEventListener("click", removeParent, false);
+  evt.target.parentNode.remove();
+};
 
 //check or uncheck list items
 
-function checked(event) {
-	if (event.target.tagName === "LI") {
-		event.target.classList.toggle("done");
-	}
-}
+const checked = event => {
+  if (event.target.tagName === "LI") {
+    event.target.classList.toggle("done");
+  }
+};
 
-//get the buttons array 
+//get the buttons array
 
-function deleteBtnsLength() {
-	console.log(deleteBtn.length);
-	return deleteBtn.length
-}
+const deleteBtnsLength = () => {
+  console.log(deleteBtn.length);
+  return deleteBtn.length;
+};
 
 // get the length of the user input
 
- function inputLength() {
- 	return input.value.length;
- };
+const inputLength = () => {
+  return input.value.length;
+};
 
+//create a new list item for user input
 
- 
+const createElement = () => {
+  // add user input to list and set input value to an empty string again
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(input.value));
+  ul.appendChild(li);
+  input.value = "";
 
+  //create delete button for each new list item
+  const btn = document.createElement("button");
+  btn.appendChild(document.createTextNode("Delete!"));
+  li.appendChild(btn);
 
-
- //create a new list item for user input
-
- function createElement() {
- 	// add user input to list and set input value to an empty string again
- 	var li = document.createElement("li");
-	    li.appendChild(document.createTextNode(input.value));
-		ul.appendChild(li);
-		input.value = "";
-		
-		//create delete button for each new list item
-	var btn = document.createElement("button");
-		btn.appendChild(document.createTextNode("Delete!"));
- 		li.appendChild(btn);
-
- 		//why is that not working if I put () behind removeParent? As we want to execute the function?
- 		btn.onclick = removeParent;
- 		
- 		
- 		
- };
+  //why is that not working if I put () behind removeParent? As we want to execute the function?
+  btn.onclick = removeParent;
+};
 
 //user click add input
-function addElementByClick() {
-	if (inputLength() > 0) {
-		createElement();
-        
-	}
+const addElementByClick = () => {
+  if (inputLength() > 0) {
+    createElement();
+  }
 };
 
 //user press enter on keyboard
-function addElementByKeypress(event) {
-    if (inputLength() > 0 && event.keyCode === 13) { //event.which works too!
-		createElement();
-		
-	}
+const addElementByKeypress = event => {
+  if (inputLength() > 0 && event.keyCode === 13) {
+    //event.which works too!
+    createElement();
+  }
 };
 
- 
- 
- ul.addEventListener("click", checked);
- 
- button.addEventListener("click", addElementByClick);
+ul.addEventListener("click", checked);
 
- input.addEventListener("keypress", addElementByKeypress);
+button.addEventListener("click", addElementByClick);
+
+input.addEventListener("keypress", addElementByKeypress);
